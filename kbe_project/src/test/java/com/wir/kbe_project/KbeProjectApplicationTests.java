fade10c14695ee5f6d91e35ddc0a8f6267c9ff86
s-fx <fxscholler@protonmail.com>
2021-10-28 18:39:33 +0200

    init project

---
+++ kbe_project/src/test/java/com/wir/kbe_project/KbeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wir.kbe_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
