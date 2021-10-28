f49e21abdd92485a487192df0b2cad27b63933ef
s-fx <fxscholler@protonmail.com>
2021-10-28 18:31:38 +0200

    init project

---
+++ futureSheep/src/main/java/com/sheep/futureSheep/FutureSheepApplication.java
@@ -0,0 +1,13 @@
+package com.sheep.futureSheep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureSheepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureSheepApplication.class, args);
+	}
+
+}
